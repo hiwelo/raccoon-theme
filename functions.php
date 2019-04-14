00cb5b72b01e439cf0be99a7a11e76efab13967f
Damien Senger <damien@raccoon.studio>
2019-04-14 23:16:54 +0200

    Updates of navigations

--- functions.php
+++ functions.php
@@ -3,6 +3,9 @@
 // Bootstrapping theme
 require_once __DIR__ . '/app/bootstrap.php';
 
+// Register navigations
+require_once __DIR__ . '/navigation.php';
+
 // Register blocks for Gutenberg
 require_once __DIR__ . '/blocks.php';
 
